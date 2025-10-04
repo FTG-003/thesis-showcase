c9634aa14383d3466b9413b85e6d6fe824c7d789
FTG-003 <siar@member.fsf.org>
2025-10-04 11:21:15 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "next-themes"
 import App from './App.tsx'
 import './index.css'
 
